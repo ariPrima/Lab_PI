206ccf696b963470e93b631535a5601716fd6d0a
Ari Prima Pandiangan <ariprimapandiangan@gmail.com>
2021-04-22 02:31:20 +0700

    Inisialisasi function kurang

--- kalkulasi.py
+++ kalkulasi.py
@@ -4,11 +4,14 @@ def bagi(x, y):
     hasil = x / y
 
     return hasil
-def kurang():
-    pass
+def kurang(x, y):
+    hasil = x - y
+
+    return hasil
 def kali():
     pass
 
 a = 5
 b = 3
 print("Hasil bagi : ", bagi(a, b))
+print("Hasil kurang :", kurang(a, b) )
